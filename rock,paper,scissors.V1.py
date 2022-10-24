8075b04c7f059b6df7321c652c818098e5dfa2b7
evansj2573 <112596071+evansj2573@users.noreply.github.com>
2022-10-25 09:29:45 +1300

    Add files via upload

--- rock,paper,scissors.V1.py
+++ rock,paper,scissors.V1.py
@@ -1,18 +1,34 @@
 
 def choice_checker(question):
 
+    error = "please choose from rock / paper / " \
+            "scissors (or xxx to quit)"
+
     valid = False
     while not valid:
 
+        response = input(question).lower()
 
-        responce = input(question)
+        if response == "r" or response == "rock":
+            return response
+        elif response == "p" or response == "paper":
+            return response
+        elif response == "s" or response == "scissors":
+            return response
 
-        if responce == "r" or responce == "rock":
-            return responce
+        elif response == "xxx":
+            return response
+        else:
+            print(error)
 
 
 
+user_choice = ""
+while user_choice != "xxx":
+
+    user_choice = choice_checker("choose rock / paper / "
+                                 "scissors (r/p/s): ")
+
+    print("you chose {}".format(user_choice))
 
 
-user_choice = choice_checker("choose rock / paper / "
-                             "scissors (r/p/s) : ")
